0a0e288f2d5e71055de806c8970a4baf559f8e68
Chris Quattrocchi and John Sawiris <john@>
2017-10-19 09:23:13 -0700

    hideous voting website

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
-alert("CONNECTED")
+$(document).ready(function(){
+  var age = parseInt(prompt("What is your age?"));
+
+  if (age > 18) {
+    $("#over-18").show();
+  } else if (age === 18) {
+    $("#over-18").show();
+  } else {
+    $("#under-18").show();
+  }
+});//doc ready function end
